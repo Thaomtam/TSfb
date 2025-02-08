0595cd39ee941d29fc687d9560632ebe1c45c9aa
Thaomtam <mtem16557@gmail.com>
2025-02-08 19:24:34 +0700

    fix

--- app/src/main/java/com/tsfb/app/MainActivity.kt
+++ app/src/main/java/com/tsfb/app/MainActivity.kt
@@ -2,36 +2,23 @@ package com.tsfb.app
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.fragment.app.Fragment
-import com.google.android.material.bottomnavigation.BottomNavigationView
-import com.tsfb.app.fragments.*
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import com.tsfb.app.databinding.ActivityMainBinding
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-        val bottomNav = findViewById<BottomNavigationView>(R.id.bottom_navigation)
-        bottomNav.setOnItemSelectedListener { item ->
-            when (item.itemId) {
-                R.id.nav_dashboard -> loadFragment(DashboardFragment())
-                R.id.nav_post -> loadFragment(PostConfigFragment())
-                R.id.nav_interaction -> loadFragment(AutoInteractionFragment())
-                R.id.nav_logs -> loadFragment(LogsFragment())
-                R.id.nav_settings -> loadFragment(SettingsFragment())
-            }
-            true
-        }
-
-        // Load dashboard by default
-        loadFragment(DashboardFragment())
-    }
-
-    private fun loadFragment(fragment: Fragment) {
-        supportFragmentManager.beginTransaction()
-            .replace(R.id.fragment_container, fragment)
-            .commit()
+        val navHostFragment = supportFragmentManager
+            .findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        val navController = navHostFragment.navController
+        binding.bottomNavigation.setupWithNavController(navController)
     }
 } 
